1e947c014d5bb1bdf8289ee9e0139af3052e96f7
singhsterabhi <singhsterabhi@gmail.com>
2019-06-23 22:12:22 +0530

    added redux

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -4,6 +4,7 @@ import classes from "./Layout.module.css";
 import Header from "../components/Header/Header";
 import Controls from "../components/Controls/Controls";
 import Products from "../components/Products/Products";
+import Cart from "../components/Cart/Cart";
 
 class Layout extends Component {
   state = {};
@@ -11,6 +12,7 @@ class Layout extends Component {
     return (
       <div className={classes.Layout}>
         <Header />
+        <Cart />
         <main className={classes.Main}>
           <Controls />
           <Products />
---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import classes from "./Cart.module.css";
+
+class Cart extends Component {
+  state = {};
+  render() {
+    return (
+      <div className={classes.Cart}>
+        <h1>Cart</h1>
+      </div>
+    );
+  }
+}
+
+export default Cart;
